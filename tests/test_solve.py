c33c0b088c041885fc064a5b4e92a4fb64b25344
Sprongion <jakeppaton@gmail.com>
2021-06-18 19:00:21 +0100

    Add import functionality

--- tests/test_solve.py
+++ tests/test_solve.py
@@ -1,5 +1,5 @@
 import pytest
-from ..src.Sudoku import Sudoku
+from ..src import Sudoku
 
 # testing cases taken from https://www.kaggle.com/bryanpark/sudoku
 with open("test_inputs.txt") as file:
@@ -13,3 +13,15 @@ def test_solve(test_input, test_expected):
     sudoku = Sudoku(test_input)
     sudoku.solve()
     assert sudoku == test_expected
+
+def test_invalid_input1():
+    with pytest.raises(Exception) as e_info:
+        sudoku = Sudoku("110000000000000000000000000000000000000000000000000000000000000000000000000000000")
+
+def test_invalid_input2():
+    with pytest.raises(Exception) as e_info:
+        sudoku = Sudoku("0000000000000000000000000000abc00000000000000000000000000000000000000000000000000")
+
+def test_invalid_input3():
+    with pytest.raises(Exception) as e_info:
+        sudoku = Sudoku("0000000000000000000000")
